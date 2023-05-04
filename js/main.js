2cc7290c170999af35b38dc099578e387105d704
Pablo <pablovilasboas02@gmail.com>
2023-05-03 22:33:53 -0300

    site 2.0

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const btnMobile = document.getElementById("btn-mobile");
+const nav = document.getElementById("nav");
+const a = nav.querySelectorAll('a')
+
+function toggleMenu(event) {
+    if (event.type === "touchstart") event.preventDefault();
+
+
+
+    nav.classList.toggle("active");
+
+    const active = nav.classList.contains("active");
+
+    // event.currentTarget.setAttribute("aria-expanded", active);
+
+    if (active) {
+        event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+    } else {
+        event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+    }
+
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
+
+a.forEach((e) => {
+    e.addEventListener('click', toggleMenu)
+})
