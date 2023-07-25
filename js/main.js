f116b57d904ca380c5525adca954d57f87c51c09
Pablo <pablovilasboas02@gmail.com>
2023-07-25 19:08:16 -0300

    ...

--- js/main.js
+++ js/main.js
@@ -27,3 +27,58 @@ btnMobile.addEventListener("touchstart", toggleMenu);
 a.forEach((e) => {
     e.addEventListener('click', toggleMenu)
 })
+
+function initScrollSuave() {
+    function scrollToSection(event) {
+        event.preventDefault()
+        const href = event.currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+        const topo = section.offsetTop
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+
+        // Forma Alternativa
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: 'smooth',
+        // })
+    }
+
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+    linksInternos.forEach(link => {
+        link.addEventListener('click', scrollToSection)
+    })
+}
+
+initScrollSuave()
+
+
+const sections = document.querySelectorAll('.js-scroll');
+console.log(sections)
+
+function animacaoScroll() {
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.7
+        function animaScroll() {
+            sections.forEach(section => {
+                const sectionTop = section.getBoundingClientRect().top
+                const isSection = (sectionTop - windowMetade) < 0
+                if (isSection) {
+                    section.classList.add('ativo')
+                }
+            })
+        }
+
+        animaScroll()
+
+        window.addEventListener('scroll', animaScroll)
+    }
+
+
+}
+
+
+animacaoScroll()
